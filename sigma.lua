29a61601f06dcee2203e0f17f76c67386169f8f8
Wizzy <108202826+Wiizzzyy@users.noreply.github.com>
2024-07-09 08:54:35 -0400

    Update sigma.lua

--- sigma.lua
+++ sigma.lua
@@ -1,3 +1,2 @@
 ---- LOOKIN' HACKABLE ----
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Wiizzzyy/hoster/main/hellmetStart.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Wiizzzyy/hoster/main/hellmetFarmer.lua'))()
